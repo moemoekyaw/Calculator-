e2000a66884a00c8c716c1ecd986e896ccadf91d
moemoekyaw <43988201+moemoekyaw@users.noreply.github.com>
2018-10-16 07:22:19 +0100

    Update Calculator.rb

--- Calculator.rb
+++ Calculator.rb
@@ -1,40 +1,35 @@
 ans="Y"
 loop do
     puts "Enter first number:"
-    num1 =gets.to_i
+    num1 =gets.chomp().to_f
 
     puts "Enter operator:"
-    op = gets.chomp
+    op = gets.chomp()
 
     puts "Enter second number:"
-    num2 =gets.to_i
+    num2 =gets.chomp().to_f
 
     if op == "+" 
     puts (num1 + num2)
-    puts result
 
     elseif op == "-"
     puts (num1 - num2)
-    puts result
 
     elseif op == "*"
     puts (num1 * num2)
-    puts result
 
     elseif op == "/" 
     puts (num1 / num2)
-    puts result
 
     elseif op=="%"
     puts (num1 % num2)
-    puts result
 
     else
     puts "Invalid"
     end
 
     puts "Do you want to repeat:Y/N?"
-    ans= gets.chomp
+    ans= gets.chomp().to_s
 break if(ans=="N")
 end
 
